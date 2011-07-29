44f187301dff404f6d81dadadf26ab052ef46e4a
Vinay Sanjekar <vinay.cs07@gmail.com>
2011-07-28 17:04:59 -0700

    Create a directory structure for the code.

---
+++ src/Node.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Node.h"
+
+Node::Node(){
+	
+	cout<<"Hi, I am a Node"<<endl;
+}
---
+++ src/Node.h
@@ -0,0 +1,15 @@
+/*
+ * Nodes and edges are the basic components of any graph. (or hypergraph).
+ * Here a node can represent anything. It has a node identifier and a 
+ * description.
+ */
+
+class Node{
+
+	// Node id should be a generic type.
+	int nid;
+	String desc;
+
+	public:
+	    Node();
+};
